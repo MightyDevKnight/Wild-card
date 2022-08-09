ec38e67e7d9e10c33b38bd830f1a15c38f1c92e0
Dracula <102849364+Phantomlancer123@users.noreply.github.com>
2022-08-09 18:57:03 +0200

    change structure
    Co-Authored-By: MightyDevKnight <102773603+MightyDevKnight@users.noreply.github.com>

--- internal/domain/binance-api/mysterybox.go
+++ internal/domain/binance-api/mysterybox.go
@@ -31,3 +31,13 @@ type NftMysteryBoxResponse struct {
 	ProductID     string `json:"productId"`
 	MappingStatus int    `json:"mappingStatus"`
 }
+type NftMysteryBoxesInfoResponse struct {
+	Data NftMysteryBoxInfoResponse `json:"data"`
+}
+
+type NftMysteryBoxInfoResponse struct {
+	StartTime    int64  `json:"startTime"`
+	Price        string `json:"price"`
+	LimitPerTime uint64 `json:"limitPerTime"`
+	UserBalance  string `json:"userBalance"`
+}
